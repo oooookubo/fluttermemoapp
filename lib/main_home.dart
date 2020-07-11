500db126a0b3469edba506d9b2fe760f68ce1eba
kuboken <kuboken0530@gmail.com>
2020-07-12 02:57:33 +0900

    firebaseのデータベース連携確認

--- lib/main_home.dart
+++ lib/main_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/book_list_page.dart';
 
 class NoteAppHome extends StatefulWidget {
   NoteAppHome({Key key, this.title}) : super(key: key);
@@ -16,13 +17,15 @@ class _NoteAppHomeState extends State<NoteAppHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
 
         floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => BookList()),
+            );
             // Add your onPressed code here!
           },
           label: Text('new page'),
